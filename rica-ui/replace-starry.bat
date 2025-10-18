a50a5d897b973f644ac875c16060fa2544071b21
kelsoko1 <elvinasenga@gmail.com>
2025-10-18 12:43:19 +0000

    better implementation of the above

--- rica-ui/replace-starry.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-del /F /Q "src\components\StarrySidebar.js"
-timeout /t 1 /nobreak > nul
-copy /Y "src\components\StarrySidebarClean.js" "src\components\StarrySidebar.js"
-echo File replaced successfully
--- rica-ui/start-mock-api.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Starting Rica Device Linking Mock API Server...
-npm install express cors uuid
-node mock-api-server.js
